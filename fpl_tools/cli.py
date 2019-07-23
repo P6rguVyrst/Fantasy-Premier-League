9506fb7267ac119603c94acfafa74d2834c1c18b
Toomas Ormisson <toomas.ormisson@gmail.com>
2019-07-23 22:12:48 +0300

    made package pip installable, moved history data to the installed package, created a cli entrypoint, added structure etc.

---
+++ fpl_tools/cli.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""CLI for interacting with FPL tools."""
+
+
+import sys
+
+
+def main():
+    print("Hello")
+
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+
+from setuptools import setup, find_packages
+
+
+requirements = ["Click>=6.0", "pandas", "requests"]
+setup_requirements = ["pytest-runner"]
+test_requirements = ["pytest"]
+
+setup(
+    author="Vaastav Anand",
+    author_email="",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    description="",
+    entry_points={"console_scripts": ["fpl-cli=fpl_tools.cli:main"]},
+    install_requires=requirements,
+    license="MIT License",
+    long_description="",
+    include_package_data=True,
+    keywords="fpl premierleague fantasypremierleague",
+    name="fpl_tools",
+    packages=find_packages(include=["fpl_tools"]),
+    setup_requires=setup_requirements,
+    test_suite="tests",
+    tests_require=test_requirements,
+    url="https://github.com/vaastav/Fantasy-Premier-League",
+    version="0.1.0",
+    zip_safe=False,
+)
